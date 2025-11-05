e2fe7db62ec92341a913bb75b00c4f64a18a785a
fanfly <fanfly@pm.me>
2025-11-05 23:00:19 +0800

    Implement UniquePointer

--- test_wa.cpp
+++ test_wa.cpp
@@ -1,5 +1,6 @@
 #include "wa.h"
 #include <gtest/gtest.h>
+#include <utility>
 
 TEST(Numeric, Gcd) {
     EXPECT_EQ(gcd(1, 0), 1);
@@ -11,4 +12,49 @@ TEST(Numeric, Gcd) {
     EXPECT_EQ(gcd(4, -6), 2);
     EXPECT_EQ(gcd(-4, 6), 2);
     EXPECT_EQ(gcd(-4, -6), 2);
+}
+
+TEST(UniquePointer, Creation) {
+    UniquePointer<int> p;
+}
+
+TEST(UniquePointer, BooleanConversion) {
+    UniquePointer<int> p;
+    EXPECT_FALSE(p);
+
+    UniquePointer<int> q{new int{0}};
+    EXPECT_TRUE(q);
+}
+
+TEST(UniquePointer, Assignment) {
+    UniquePointer<int> p{new int{0}};
+    UniquePointer<int> q{new int{1}};
+
+    p = std::move(q);
+    EXPECT_TRUE(p);
+    EXPECT_FALSE(q);
+    EXPECT_EQ(*p, 1);
+}
+
+TEST(UniquePointer, Reset) {
+    UniquePointer<int> p;
+    EXPECT_FALSE(p);
+
+    p.reset(new int{1});
+    EXPECT_TRUE(p);
+    EXPECT_EQ(*p, 1);
+
+    p.reset();
+    EXPECT_FALSE(p);
+}
+
+TEST(UniquePointer, Release) {
+    UniquePointer<int> p{new int{1}};
+    EXPECT_TRUE(p);
+
+    int *q = p.release();
+    EXPECT_FALSE(p);
+    EXPECT_EQ(*q, 1);
+
+    delete q;
 }
--- wa.h
+++ wa.h
@@ -19,4 +19,39 @@ auto gcd(T m, U n) -> decltype(T{} + U{}) {
     return a;
 }
 
+template <typename T>
+class UniquePointer {
+  public:
+    using element_type = T;
+    using pointer = T *;
+
+    UniquePointer() : p_{nullptr} {}
+    explicit UniquePointer(pointer p) : p_{p} {}
+    UniquePointer(const UniquePointer &other) = delete;
+    UniquePointer(UniquePointer &&other) : p_{other.p_} { other.p_ = nullptr; }
+    UniquePointer &operator=(const UniquePointer &other) = delete;
+    UniquePointer &operator=(UniquePointer &&other) {
+        delete p_;
+        p_ = other.p_;
+        other.p_ = nullptr;
+        return *this;
+    }
+    ~UniquePointer() { delete p_; }
+
+    pointer get() const { return p_; }
+    element_type &operator*() const { return *p_; }
+    pointer operator->() const { return p_; }
+    explicit operator bool() const { return p_ != nullptr; }
+
+    void reset(pointer p = pointer{}) { p_ = p; }
+    pointer release() {
+        pointer p = p_;
+        p_ = nullptr;
+        return p;
+    }
+
+  private:
+    T *p_;
+};
+
 #endif // WA_H_
